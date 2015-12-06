f963534a5075685edcb44aaed5115123d63a966f
Hiroki Narita <narita@kanamei.co.jp>
2015-12-06 14:28:17 +0900

    first commit
    Signed-off-by: Hiroki Narita <narita@kanamei.co.jp>

---
+++ reciever.js
@@ -0,0 +1,38 @@
+var Bleacon = require('bleacon');
+//var execSync = require('child_process').execSync;
+var Sound = require('node-mpg123');
+
+var fami = new Sound('famima.mp3');
+var playing=false;
+var min = 0, max = -100;
+Bleacon.startScanning();
+
+Bleacon.on('discover', function(bleacon) {
+   //console.dir(bleacon);
+  // console.log(bleacon.rssi)
+  var rssi = bleacon.rssi;
+  var old_min = min;
+  var old_max = max;
+  min = Math.min(rssi,min);
+  max = Math.max(rssi,max);
+ 
+ if(min < old_min){ 
+  console.log("min: "+ min);
+ }
+ if(max > old_max) {
+   console.log("max: "+ max);
+ }
+  if(bleacon.rssi > -65 && playing === false){
+    //child = execSync('mpg123 famima.mp3');
+    //console.log(child);
+    //process.exit();
+      playing = true;
+      fami.play();
+ 
+ } 
+
+ fami.on("complete",function(){
+    playing = false;
+ });
+
+});
